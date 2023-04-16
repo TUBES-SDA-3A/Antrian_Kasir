d003ec3ab7516ad536d5db542c206a0dd2b66939
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2023-04-17 00:21:56 +0700

    Progress 2 Co-authored-by: HasnaFitriyaniK <HasnaFitriyaniK@users.noreply.github.com>

--- 3A/Body.c
+++ 3A/Body.c
@@ -1,9 +1,8 @@
 #include "Header.h"
 
-// List Pelanggan
-
-bool isEmpty_Pembeli(address_P p)
-{	// check
+/****************** Pembeli *******************/
+bool isEmptyPembeli(address_P p)
+{
 	/* 	I.S : tidak ada yang berubah, hanya menerima satu parameter berupa alamat dari node pembeli.
 		F.S : fungsi ini mengembalikan nilai boolean, yaitu 1 jika queue pembeli kosong dan 0 jika queue pembeli tidak kosong.
 	*/
@@ -18,8 +17,8 @@ bool isEmpty_Pembeli(address_P p)
 	}
 }
 
-void Create_Node_Pembeli(address_P *headPembeli, address_P *newPembeli, address_BB *q, infochar Nama_Pembeli, int No_Kasir, int uangPembeli)
-{	
+void createNodePembeli(address_P *headPembeli, address_P *newPembeli, address_BB *q, infochar Nama_Pembeli, int No_Kasir, int uangPembeli)
+{
 	/* 	I.S : headPembeli dan newPembeli belum memiliki nilai atau masih kosong, dan address_BB q, infochar Nama_Pembeli, int No_Kasir, dan int uangPembeli diisi dengan nilai yang sesuai.
 		F.S : terbentuknya sebuah node baru pada linked list pembeli dengan informasi yang sudah diisi dan disambungkan ke linked list.
 	*/
@@ -40,14 +39,14 @@ void Create_Node_Pembeli(address_P *headPembeli, address_P *newPembeli, address_
 		(*newPembeli)->hargaTotal = 0;
 		(*newPembeli)->uangPembeli = uangPembeli;
 	}
-	if (isEmpty_Pembeli(*headPembeli))
+	if (isEmptyPembeli(*headPembeli))
 	{
 		*headPembeli = *newPembeli;
 	}
 	else
 	{
 		Traversal = *headPembeli;
-		while (!isEmpty_Pembeli(Traversal->next))
+		while (!isEmptyPembeli(Traversal->next))
 		{
 			Traversal = Traversal->next;
 		}
@@ -55,11 +54,19 @@ void Create_Node_Pembeli(address_P *headPembeli, address_P *newPembeli, address_
 	}
 }
 
+/****************** Persediaan Barang *****************/
+void persediaanBarang(DataBarang dataBarang[MAX_BARANG])
+{
+	dataBarang[0].nama = "Shampo", dataBarang[0].stok = 50, dataBarang[0].harga = 15000;
+	dataBarang[1].nama = "Sabun Cuci Piring";
+	dataBarang[2].nama = "Sendok & Garpu";
+	dataBarang[3].nama = "Bubur Bayi";
+	dataBarang[4].nama = "Susu UHT";
+}
 
-/*************** Barang Belian ***************/
-
-bool isEmpty_BarangBelian(address_BB p)
-{	// check
+/****************** Barang Belian *******************/
+bool isEmptyBarangBelian(address_BB p)
+{
 	/* I.S : Alamat node barang belian (p) mungkin kosong (NULL) atau tidak kosong.
 	   F.S : mengetahui apakah suatu node barang belian kosong atau tidak dengan melihat nilai boolean yang dikembalikan oleh fungsi ini.
 	*/
@@ -74,8 +81,8 @@ bool isEmpty_BarangBelian(address_BB p)
 	}
 }
 
-void Create_Node_BarangBelian(address_BB *headBarangBelian, address_BB *newBarangBelian, infochar nama_barang, int jumlah_barang)
-{	// check
+void createNodeBarangBelian(address_BB *headBarangBelian, address_BB *newBarangBelian, infochar nama_barang, int jumlah_barang)
+{ // check
 	/* I.S : - headAntrian merupakan pointer ke list antrian yang awalnya kosong atau NULL.
 			 - newAntrian merupakan pointer yang belum dialokasikan ke memori.
 			 - Pembeli merupakan data pembeli yang akan ditambahkan ke dalam node antrian.
@@ -85,8 +92,8 @@ void Create_Node_BarangBelian(address_BB *headBarangBelian, address_BB *newBaran
 	address_BB transBB;
 
 	*newBarangBelian = (address_BB)malloc(sizeof(List_BarangBelian));
-	
-	if (isEmpty_BarangBelian(*newBarangBelian))
+
+	if (isEmptyBarangBelian(*newBarangBelian))
 	{
 		printf("Node Belum di Alokasi\n");
 	}
@@ -97,234 +104,38 @@ void Create_Node_BarangBelian(address_BB *headBarangBelian, address_BB *newBaran
 		(*newBarangBelian)->jumlahBarang = jumlah_barang;
 	}
 
-	if(isEmpty_BarangBelian(*headBarangBelian)){
+	if (isEmptyBarangBelian(*headBarangBelian))
+	{
 		*headBarangBelian = *newBarangBelian;
-	}else{
+	}
+	else
+	{
 		transBB = *headBarangBelian;
-		while(!isEmpty_BarangBelian(transBB->next)){
+		while (!isEmptyBarangBelian(transBB->next))
+		{
 			transBB = (transBB)->next;
 		}
-		transBB = newBarangBelian;
- 	}
+		transBB = *newBarangBelian;
+	}
 }
 
-void Del_Awal_BarangBelian(address_BB *p, infochar *X)
-{	// check
+void delAwalBarangBelian(address_BB *barangBelian)
+{
 	/* I.S : Terdapat sebuah node yang menjadi inputan fungsi Del_Awal_BarangBelian, dan pada awalnya node tersebut menunjuk ke head dari sebuah list.
 	   F.S : Node pertama dari list telah dihapus dan nilai nama barang dari node tersebut disimpan pada variabel X.
 	*/
 
-	address_BB A;
+	address_BB tempBarangBelian;
 
-	if (isEmpty_BarangBelian(*p))
+	if (isEmpty_BarangBelian(*barangBelian))
 	{
 		printf("List Kosong");
 	}
 	else
 	{
-		A = *p;
-		*X = Nama_Barang(A);
-		*p = next(*p);
-		A = NULL;
-		free(A);
+		tempBarangBelian = *barangBelian;
+		*barangBelian = next(*barangBelian);
+		tempBarangBelian = NULL;
+		free(tempBarangBelian);
 	}
-}
-
-// List Antrian
-bool isEmpty_Antrian(address_A p)
-{	// check
-	/* I.S : belum mengetahui apakah address_A kosong atau tidak.
-	   F.S : address_A p merupakan alamat dari head dari suatu linked list address_A yang akan dicek apakah kosong atau tidak.
-	*/
-
-	if (p == NULL)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
-
-void Create_Node_Antrian(address_A *headAntrian,address_A *newAntrian, address_P Pembeli)
-{	// check
-	/* I.S : - headAntrian: pointer menuju node pertama dari list antrian
-			 - newAntrian: pointer untuk menunjuk pada node baru yang akan dibuat
-			 - Pembeli: pointer menuju pada node pembeli
-	   F.S : Jika newAntrian berhasil dialokasikan, maka newAntrian akan menjadi node baru pada list antrian yang menunjuk ke Pembeli.
-			 Jika headAntrian masih kosong, maka headAntrian akan menunjuk ke newAntrian.
-			 Jika headAntrian tidak kosong, maka newAntrian akan ditambahkan pada akhir list antrian.
-	*/
-
-	address_A transA;
-
-	*newAntrian = (address_A)malloc(sizeof(Antrian)); // membuat node sebesar list antrian
-	
-	if (isEmpty_Antrian(*newAntrian))
-	{
-		printf("Node Belum di Alokasi\n");
-	}
-	else
-	{
-		(*newAntrian) = Nil;				 // next yang ditunjuk p disii Nil
-		(*newAntrian)->Pembeli = Pembeli; // nama yang ditunjuk p diisi nama
-	}
-
-	if (isEmpty_Antrian(*headAntrian))
-	{
-		*headAntrian = *newAntrian; // pointer diisi pNew
-	}
-	else
-	{
-		transA = *headAntrian;
-		while (!isEmpty_Antrian((transA)->next))
-		{				   // memeriksa next A apakah kosong
-			transA = (transA)->next; // A diisi next A
-		}
-		(transA)->next = newAntrian;
-	}
-}
-
-void Tampil_List_Antrian(address_A p)
-{
-	// Menampilkan seluruh isi Linked List, p parameter passing by value
-	int i = 1;
-
-	if (isEmpty_Antrian(p))
-	{
-		printf("List Kosong\n");
-	}
-	else
-	{
-		while (!isEmpty_Antrian(p))
-		{
-			printf("%d. %s", i, p->Pembeli->namaPembeli); // print yang ada di antrian
-			p = next(p);								  // p diisi next p
-			if (p != Nil)
-			{ // jika p tidak sama dengan nil
-				printf("\n");
-			}
-			i++; // lakukan ini sebanyak i
-		}
-		printf("\n");
-	}
-}
-
-void Del_Awal_Antrian(address_A *p, infochar *X)
-{
-
-	address_A A;
-
-	if (isEmpty_Antrian(*p))
-	{								// memeriksa apakah antrian kosong
-		printf("Antrian Kosong\n"); // jika kosong print
-	}
-	else
-	{
-		A = *p;		   // Antrian diisi address p
-		*X = nama(A);  // mengalokasi dengan nama pada Antrian
-		*p = next(*p); // pointer diisi next p
-		A = NULL;	   // A diisi NIL
-		free(A);	   // hampus A dari antrian
-	}
-}
-
-void DelPBarangBelian(address_BB *BB, infochar X)
-/* IS : L sembarang */
-/* FS : Jika ada elemen list beraddress P, dengan Info(P) = X */
-/* 	Maka P dihapus dari list dan di dealokasi */
-/* Jika tidak ada elemen list dengan Info(P) = X, maka list tetap */
-/* List mungkin menjadi kosong karena penghapusan */
-{
-	/* Kamus Lokal */
-	address_BB TransBB, Prec;
-	bool found = false;
-	/* Algoritma */
-	Prec = Nil;
-	TransBB = *BB;
-	while ((TransBB != Nil) && (!found))
-	{
-		if (strcmp((TransBB)->namaBarang, X) == 0)
-		{
-			found = true;
-		}
-		else
-		{
-			Prec = TransBB;
-			TransBB = (TransBB)->next;
-		}
-	} 
-
-	if (found)
-	{
-		if (Prec == Nil && (TransBB)->next == Nil) /* Hanya 1 elemen */
-		{
-			TransBB->next = Nil;
-		}
-		else if (Prec == Nil) /* Ketemu di elemen 1*/
-		{
-			*BB = (TransBB)->next;
-			TransBB->next = Nil;
-		}
-		else /* Ketemu di elemen list yang ditengah/akhir */
-		{
-			(Prec)->next = (TransBB)->next;
-			TransBB->next = Nil;
-		}
-		DeAlokasi(TransBB);
-	}
-}
-
-void DelPAntrian(address_P *Pembeli, address_A next)
-/* IS : L sembarang */
-/* FS : Jika ada elemen list beraddress P, dengan Info(P) = X */
-/* 	Maka P dihapus dari list dan di dealokasi */
-/* Jika tidak ada elemen list dengan Info(P) = X, maka list tetap */
-/* List mungkin menjadi kosong karena penghapusan */
-{
-	/* Kamus Lokal */
-	address_P  Prec, transP;
-	bool found = false;
-	/* Algoritma */
-	Prec = Nil;
-	transP = (*Pembeli);
-	while ((transP != Nil) && (!found))
-	{
-		if (strcmp((transP)->namaPembeli, next) == 0)
-		{
-			found = true;
-		}
-		else
-		{
-			Prec = transP;
-			transP = (transP)->next;
-		}
-	} 
-
-	if (found)
-	{
-		if (Prec == Nil && (transP)->next == Nil) /* Hanya 1 elemen */
-		{
-			(transP)->next = Nil;
-		}
-		else if (Prec == Nil) /* Ketemu di elemen 1*/
-		{
-			(*Pembeli) = (transP)->next;
-			(transP)->next = Nil;
-		}
-		else /* Ketemu di elemen list yang ditengah/akhir */
-		{
-			(Prec)->next = (transP)->next;
-			(transP)->next = Nil;
-		}
-		DeAlokasi(transP);
-	}
-}
-
-void Dequeue_Antrian(address_A *Q, infochar *X)
-{
-	// memberikan nomor antrian dan nama di antrian
-
-	Del_Awal_Antrian(&(*Q), &(*X)); // Q disini bertipe address A, dan X passing parameter by address
 }
